cb6bfcb1234c89eeaa05f8f53c56cb7705c8a3ed
andrea scacchi <andrea.scacchi@live.com>
2022-05-12 16:02:39 +0200

    MidInitial commit, palyndrom check done via console.log now onto DOM manipulation

--- js/script-01.js
+++ js/script-01.js
@@ -2,3 +2,27 @@
 // Verificare che la parola sia palindroma
 
 //first console.log
+
+const wordGiven = prompt("Inserisci una parola");
+const wordGivenArray = wordGiven.split("");
+
+const wordReverseArray = wordGivenArray.reverse();
+const wordReverse = wordReverseArray.join("");
+
+
+// console log checks, why the array is reversed?
+console.log("word given was " + wordGiven);
+console.log(wordGivenArray);
+console.log(wordReverseArray);
+console.log("word revers is " + wordReverse);
+
+
+// check palyndrom conditions
+if (wordGiven === wordReverse){
+    console.log("The word " + wordGiven + " is a Palyndrom");
+} else {
+    console.log("The word " + wordGiven + " is NOT a Palyndrom");
+}
+
+
+//now onto input
