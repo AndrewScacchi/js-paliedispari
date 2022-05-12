c8e20304cebb9c3e10b56b3e15951526a4e25592
andrea scacchi <andrea.scacchi@live.com>
2022-05-12 18:52:02 +0200

    Final Commit. Added a few proof of concept checks on the user input. Not supposed to be exaustive. it's a pain in the arse to creare checks on checks on checks

--- js/script-02.js
+++ js/script-02.js
@@ -29,23 +29,63 @@ function additionCheck(numUser, numPc){
 // al click del bottone, prendo il valore dell'input, lo fornisco alla funzione prima stabilita, metto il risultato sul DOM
 button.addEventListener("click", function(){
     const userNumber = document.getElementById("input-number").value;
-    const pcNumber =  Math.floor(Math.random() * 5 + 1 );
-    const finalAddition = parseInt(userNumber) + parseInt(pcNumber);
+    const choiceEven = document.getElementById("even");
+    const choiceOdd = document.getElementById("odd");
 
-    const result = additionCheck(userNumber, pcNumber);
-    const flavourText = document.getElementById("flavour-text");
-    flavourText.innerHTML = `
-        Il tuo numero era ${userNumber}; <br>
-        Il numero del PC era ${pcNumber}; <br>
-        `
-    if (result){
-        flavourText.innerHTML += `
-        La somma ${finalAddition} è un numero PARI.
-         `
+    // check if radio buttons are selected
+    if (!choiceEven.checked && !choiceOdd.checked){
+        alert("Scegli se il risultato è pari o dispari!")
     } else {
-        flavourText.innerHTML += `
-        La somma ${finalAddition} è un numero DISPARI.
-         `
+
+        //CHECK NUMBER RANGE ON BUTTON CLICK 
+        if (userNumber < 1 || userNumber > 5 ){
+            alert("Il numero inserito non è nel range richiesto!");
+    
+        } else if (isNaN(userNumber)){
+            alert("Quello non è un numero!");
+        } else {
+    
+            //NOW DO THE MAJOR CODE
+            
+            const pcNumber =  Math.floor(Math.random() * 5 + 1 );
+            const finalAddition = parseInt(userNumber) + parseInt(pcNumber);
+        
+            const result = additionCheck(userNumber, pcNumber);
+            const flavourText = document.getElementById("flavour-text");
+            flavourText.innerHTML = `
+                Il tuo numero era ${userNumber}; <br>
+                Il numero del PC era ${pcNumber}; <br>
+                `
+            if (result){
+                flavourText.innerHTML += `
+                La somma ${finalAddition} è un numero PARI. <br>
+                 `
+            } else {
+                flavourText.innerHTML += `
+                La somma ${finalAddition} è un numero DISPARI. <br>
+                 `
+            }
+
+
+            // FINAL DISCRIMATION
+            if (choiceEven.checked && result){
+                flavourText.innerHTML += `
+                <strong>Hai indovinato il risultato!</strong>
+                `
+            } else if (choiceOdd.checked && !result){
+                flavourText.innerHTML += `
+                <strong>Hai indovinato il risultato!</strong>
+                `
+            } else {
+                flavourText.innerHTML += `
+                <strong>Non hai indovinato il risultato!</strong>
+                `
+            }
+
+        }
     }
+
+
+
 
 })
