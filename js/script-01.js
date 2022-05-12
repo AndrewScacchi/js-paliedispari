fa630d791f1fe72d6578b322cde5860492fba216
andrea scacchi <andrea.scacchi@live.com>
2022-05-12 16:56:45 +0200

    Exercise on Palyndrome completed, given basic styles and dom element, created function called on buttonclick

--- js/script-01.js
+++ js/script-01.js
@@ -2,7 +2,7 @@
 // Verificare che la parola sia palindroma
 
 //first console.log
-
+/*
 const wordGiven = prompt("Inserisci una parola");
 const wordGivenArray = wordGiven.split("");
 
@@ -23,6 +23,36 @@ if (wordGiven === wordReverse){
 } else {
     console.log("The word " + wordGiven + " is NOT a Palyndrom");
 }
+*/
+
+//now onto DOM manipulation
+
+var wordGiven;
+const button = document.getElementById("btn-send");
+
+function palyndromeCheck(word){
+    let wordArray = word.split("");
+    let wordReverseArray = wordArray.reverse();
+    let wordReverse = wordReverseArray.join("");
+
+    if (word === wordReverse){
+        const isPalyndrome = `La parola <span class="upperCase">\"${word}\"</span> <strong>E'</strong> un palindromo`;
+        return isPalyndrome;
+    } else {
+        const isNotPalyndrome = `La parola <span class="upperCase">\"${word}\"</span> <strong>NON E'</strong> un palindromo`;
+        return isNotPalyndrome;
+    }
+}
 
 
-//now onto input
+
+// al click del bottone, prendo il valore dell'input, lo fornisco alla funzione prima stabilita, metto il risultato sul DOM
+
+
+button.addEventListener("click", function(){
+    wordGiven = document.getElementById("input-word").value;
+    const finalresult = palyndromeCheck(wordGiven);
+    const flavourText = document.getElementById("flavour-text");
+    flavourText.innerHTML = finalresult;
+})
+
