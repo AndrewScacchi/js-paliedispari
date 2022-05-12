3c646137be8c4db237608141841fd0675bf0b936
andrea scacchi <andrea.scacchi@live.com>
2022-05-12 19:03:04 +0200

    Errata Corrige, correct spelling, eliminated repeating code.

--- js/script-02.js
+++ js/script-02.js
@@ -1,14 +1,4 @@
 // EXERCISE 02 
-/*
-User choose Even or Odd. (TO DO)
-
-User insert a number between 1 and 5 (TO DO CHECKS)
-PC generate a random number in the same range (DONE)                           
-Using a fx check if addition is ODD or EVEN, (DONE)
-declare a winner
-
-*/
-
 
 // salvo in variabili il buttone e il numero dato
 const button = document.getElementById("btn-send");
@@ -24,7 +14,6 @@ function additionCheck(numUser, numPc){
         const result = false;
         return result;
     }
-
 }
 // al click del bottone, prendo il valore dell'input, lo fornisco alla funzione prima stabilita, metto il risultato sul DOM
 button.addEventListener("click", function(){
@@ -34,24 +23,25 @@ button.addEventListener("click", function(){
 
     // check if radio buttons are selected
     if (!choiceEven.checked && !choiceOdd.checked){
-        alert("Scegli se il risultato è pari o dispari!")
+        alert("Scommetti se il risultato sarà pari o dispari!")
     } else {
 
         //CHECK NUMBER RANGE ON BUTTON CLICK 
         if (userNumber < 1 || userNumber > 5 ){
-            alert("Il numero inserito non è nel range richiesto!");
-    
+            alert("Il numero inserito non è nel range richiesto!");   
         } else if (isNaN(userNumber)){
             alert("Quello non è un numero!");
         } else {
-    
             //NOW DO THE MAJOR CODE
             
             const pcNumber =  Math.floor(Math.random() * 5 + 1 );
+            // had to repeat this variable because exist only in the function (even tho was declared as a const...)
             const finalAddition = parseInt(userNumber) + parseInt(pcNumber);
         
             const result = additionCheck(userNumber, pcNumber);
             const flavourText = document.getElementById("flavour-text");
+
+            //finally do something!
             flavourText.innerHTML = `
                 Il tuo numero era ${userNumber}; <br>
                 Il numero del PC era ${pcNumber}; <br>
@@ -66,13 +56,8 @@ button.addEventListener("click", function(){
                  `
             }
 
-
-            // FINAL DISCRIMATION
-            if (choiceEven.checked && result){
-                flavourText.innerHTML += `
-                <strong>Hai indovinato il risultato!</strong>
-                `
-            } else if (choiceOdd.checked && !result){
+            // Final discrimination, checking on user radial choice
+            if ((choiceEven.checked && result) || (choiceOdd.checked && !result)){
                 flavourText.innerHTML += `
                 <strong>Hai indovinato il risultato!</strong>
                 `
@@ -81,11 +66,6 @@ button.addEventListener("click", function(){
                 <strong>Non hai indovinato il risultato!</strong>
                 `
             }
-
         }
     }
-
-
-
-
 })
